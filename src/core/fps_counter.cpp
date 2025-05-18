fb94ea4ef1fb2834713d32748c6e8ed547f7c76f
RedShiftedDev <rupayan105das92@gmail.com>
2025-05-18 20:16:51 +0530

    ❖

--- src/core/fps_counter.cpp
+++ src/core/fps_counter.cpp
@@ -1,20 +1,3 @@
-#include "fps_counter.h"
-
-FpsCounter::FpsCounter()
-    : frameCount(0), lastTime(std::chrono::high_resolution_clock::now()), fps(0.0F) {}
-
-float FpsCounter::update() {
-  frameCount++;
-  auto currentTime = std::chrono::high_resolution_clock::now();
-  float elapsed = std::chrono::duration<float>(currentTime - lastTime).count();
-
-  if (elapsed >= 1.0F) {
-    fps = static_cast<float>(frameCount) / elapsed;
-    frameCount = 0;
-    lastTime = currentTime;
-  }
-
-  return fps;
-}
-
-float FpsCounter::getFps() const { return fps; }
+//
+// Created by Rupayan Das on 18/05/25.
+//
